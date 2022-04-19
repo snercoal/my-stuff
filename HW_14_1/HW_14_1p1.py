d23e5258552e1c53212ce73b4e9d9e795dbd780e
snercoal <elilemonduc@gmail.com>
2022-04-19 19:37:03 -0400

    finsihed

--- HW_14_1/HW_14_1p1.py
+++ HW_14_1/HW_14_1p1.py
@@ -6,6 +6,6 @@ timeElapsed = int(input("Enter the amount of time: "));
 constantA = ( (math.log(1/2) / 5730) );
 calculate = ( (initialCarbon) * math.exp(constantA * timeElapsed));
 
-for x in range(100, timeElapsed+1, 100):
-    calculatee = ( (initialCarbon) * math.exp(constantA * x))
-    print("After " +str(x)+ " years, the amount of Carbon-14 will be: " +str(calculatee));
+for year in range(100, timeElapsed+1, 100):
+    calculatee = ( (initialCarbon) * math.exp(constantA * year))
+    print("After " +str(year)+ " years, the amount of Carbon-14 will be: " +str(calculatee));
