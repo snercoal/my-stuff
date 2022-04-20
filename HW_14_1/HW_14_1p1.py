b012a4b88905241ed58a859c53395a51d265f357
snercoal <elilemonduc@gmail.com>
2022-04-19 20:10:13 -0400

    cleaned up var names

--- HW_14_1/HW_14_1p1.py
+++ HW_14_1/HW_14_1p1.py
@@ -4,8 +4,8 @@ initialCarbon = int(input("Enter the initial amount of carbon: "));
 timeElapsed = int(input("Enter the amount of time: "));
 
 constantA = ( (math.log(1/2) / 5730) );
-calculate = ( (initialCarbon) * math.exp(constantA * timeElapsed));
+currentCarbon = ( (initialCarbon) * math.exp(constantA * timeElapsed));
 
 for year in range(100, timeElapsed+1, 100):
-    calculatee = ( (initialCarbon) * math.exp(constantA * year))
-    print("After " +str(year)+ " years, the amount of Carbon-14 will be: " +str(calculatee));
+    currentCarbon = ( (initialCarbon) * math.exp(constantA * year))
+    print("After " +str(year)+ " years, the amount of Carbon-14 will be: " +str(currentCarbon));
