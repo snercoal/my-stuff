32ff0002bf51d0ff65bd86076c10839706fabcca
snercoal <elilemonduc@gmail.com>
2022-04-20 03:05:59 -0400

    finished

---
+++ HW_14_1/HW_14_1_p2.py
@@ -0,0 +1,13 @@
+dailyDeposit = float(input("Enter the amount of your initial deposit: "));
+interestRate = ((float(input("Enter your interest rate: "))) / 100);
+desiredBal = float(input("Enter your desired final Balance: "));
+currentBal = 0;
+day = 0;
+while(currentBal < desiredBal):
+    currentBal = (currentBal + (currentBal * interestRate) + dailyDeposit);
+    day += 1;
+finalBal = round(currentBal, 2)
+years = (day / 365);
+print("Your Balance of " + str(finalBal) + " will be reached in " + str(round(years, 3)) + " years");
+
+
